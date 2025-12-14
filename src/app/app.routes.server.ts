5306d9df196695c13e55df4637bae4735ed1b059
yassine <yassineabdi20@gmail.com>
2025-12-14 15:01:38 +0100

    15:01

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Render movie detail pages on-demand on the server because they use a dynamic `:id` param
+  {
+    path: 'movie/:id',
+    renderMode: RenderMode.Server
+  },
+  // Prerender other routes (catch-all)
   {
     path: '**',
     renderMode: RenderMode.Prerender
